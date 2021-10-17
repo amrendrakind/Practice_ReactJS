4d0a25c3d43e87b533ee5b51a927fccdc2fef407
amrendrakind <vision2010@gmail.com>
2021-10-17 16:32:04 +0530

    Favyt test

--- favyt/src/App.js
+++ favyt/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './frontend/component/Header'
+import Navbar from './frontend/component/Navbar'
+import Sidebar from './frontend/component/Sidebar';
+import Content from './frontend/component/Content';
+import Footer from './frontend/component/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
