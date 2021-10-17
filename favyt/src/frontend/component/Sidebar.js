d0d833b0778052540aea74fa8c77af84d696f996
amrendrakind <vision2010@gmail.com>
2021-10-17 16:22:06 +0530

    Favyt test

---
+++ favyt/src/frontend/component/Sidebar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+class Sidebar extends Component {
+    render() {
+        return (
+            <div className="side">
+                <h2>Arcu bibendum</h2>
+                <h5>Sit amet mattis vulputate</h5>
+                <div className="fakeimg" style={{ height: 200 }}>Image</div>
+                <p>Non blandit massa enim nec dui nunc mattis enim. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla..</p>
+                <h3>Massa enim</h3>
+                <p>Lorem ipsum dolor sit ame.</p>
+                <div className="fakeimg" style={{ height: 60 }}>Image</div><br />
+                <div className="fakeimg" style={{ height: 60 }}>Image</div><br />
+                <div className="fakeimg" style={{ height: 60 }}>Image</div>
+            </div>
+        )
+    }
+}
+export default Sidebar
