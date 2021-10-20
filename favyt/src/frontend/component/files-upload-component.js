1e3fd89446c133aed5ba3e62e71604fa3afe202b
amrendrakind <vision2010@gmail.com>
2021-10-20 23:56:16 +0530

    Upload Image

--- favyt/src/frontend/component/files-upload-component.js
+++ favyt/src/frontend/component/files-upload-component.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class FilesUploadComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onFileChange = this.onFileChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            profileImg: ''
+        }
+    }
+
+    onFileChange(e) {
+        this.setState({ profileImg: e.target.files[0] })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('profileImg', this.state.profileImg)
+        axios.post("http://localhost:4000/api/user-profile", formData, {
+        }).then(res => {
+            console.log(res)
+        })
+    }
+
+
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <form>
-                        <h3>React File Upload</h3>
+                    <form onSubmit={this.onSubmit}>
                         <div className="form-group">
-                            <input type="file" />
+                            <input type="file" onChange={this.onFileChange} />
                         </div>
                         <div className="form-group">
                             <button className="btn btn-primary" type="submit">Upload</button>
