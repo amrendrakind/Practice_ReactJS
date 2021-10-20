bd1b79f938b57827cd7c2b497ca5fccf3ffddb00
amrendrakind <vision2010@gmail.com>
2021-10-20 23:28:09 +0530

    Upload Image

--- favyt/src/App.js
+++ favyt/src/App.js
@@ -3,39 +3,9 @@ import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [userList, setUserList] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://reqres.in/api/users').then(res => {
-              setUserList(res.data.data);
-    });
-  }, []);
-
   return (
-    <div className="container-fluid p-3">
-      <h5 className="d-inline-block">GET request using axios with React Hooks</h5>
-      <table class="table table-sm mt-3">
-        <thead class="thead-dark">
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Avatar</th>
-        </thead>
-        <tbody>
-          {userList.map(x => <tr>
-            <td>{x.first_name}</td>
-            <td>{x.last_name}</td>
-            <td>{x.email}</td>
-            <td><img src={x.avatar} width="50" height="50" /></td>
-          </tr>)}
-          {userList.length == 0 && <tr>
-            <td className="text-center" colSpan="4">
-              <b>No data found to display.</b>
-            </td>
-          </tr>}
-        </tbody>
-      </table>
-
+    <div className="App">
+      <h2>React File Upload Demo</h2>
     </div>
   );
 }
