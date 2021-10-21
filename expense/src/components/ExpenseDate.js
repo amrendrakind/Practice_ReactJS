e42067201498233030e9e6773ddbd70c54e3f12a
amrendrakind <vision2010@gmail.com>
2021-10-21 23:04:23 +0530

    ExpenseItem

---
+++ expense/src/components/ExpenseDate.js
@@ -0,0 +1,14 @@
+import './ExpenseDate.css'
+function ExpenseDate(props) {
+    const year = props.date.getFullYear();
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    const day = props.date.toLocaleString('en-US', { month: '2-digit' })
+
+    return (
+        <div className="expense-date">
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>)
+}
+export default ExpenseDate
--- expense/src/components/ExpenseItem.js
+++ expense/src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
-    const year = props.date.getFullYear()
-    const month = props.date.toLocaleString('en-US',{month:'long'})
-    const day = props.date.toLocaleString('en-US',{month:'2-digit'})
 
     return (<div className="expense-item">
-        <div >
-            <div>{month}</div>
-            <div>{year}</div>  
-            <div>{day}</div>
-        </div>
+        <ExpenseDate />
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
