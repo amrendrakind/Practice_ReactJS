13c1089f17b5ff6b4823c92c586dea18f6af2ea3
amrendrakind <vision2010@gmail.com>
2021-10-22 07:48:46 +0530

    ExpenseItem

--- expense/src/components/ExpenseDate.js
+++ expense/src/components/ExpenseDate.js
@@ -6,9 +6,9 @@ function ExpenseDate(props) {
 
     return (
         <div className = "expense-date">
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
+            <div className = "expense-date__month">{month}</div>
+            <div className = "expense-date__year">{year}</div>
+            <div className = "expense-date__day">{day}</div>
         </div>)
 }
 export default ExpenseDate
