f97b630979d34d0b01695e322056f36771a2f6e9
amrendrakind <vision2010@gmail.com>
2021-10-24 16:24:50 +0530

    ExpenseList Filter Modified

--- expense/src/components/Expenses/Expenses.js
+++ expense/src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
-import ExpenseItem from "./ExpenseItem";
+import { useState } from "react/cjs/react.development";
+
 import Card from "../UI/Card";
 import './Expenses.css'
 import ExpenseFilter from "./ExpenseFilter";
-import { useState } from "react/cjs/react.development";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
 
@@ -13,26 +14,15 @@ function Expenses(props) {
     }
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear
-    }
+    })
 
-    )
     return (
         <Card className="expenses">
             <ExpenseFilter
                 selected={filteredYear}
                 onChangeFilter={filterChangeHandler}
             />
-            {filteredExpenses.length === 0 && <p>No Expenses Found.</p>}
-            {filteredExpenses.length > 0 &&
-                filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))
-            }
+            <ExpensesList items={filteredExpenses} />
         </Card>
     )
 }
