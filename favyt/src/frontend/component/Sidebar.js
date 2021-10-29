6dcf3f8e1520d9b57ee42e2cab0946957e65df4f
amrendrakind <vision2010@gmail.com>
2021-10-29 23:34:04 +0530

    Updated

--- favyt/src/frontend/component/Sidebar.js
+++ favyt/src/frontend/component/Sidebar.js
@@ -3,15 +3,15 @@ class Sidebar extends Component {
     render() {
         return (
             <div className="sidebar">
-                <h2>Arcu bibendum</h2>
+                <h2>Current News</h2>
                 <h5>Sit amet mattis vulputate</h5>
-                <div className="fakeimg" style={{ height: 250 }}>Image</div>
+                <div className="fakeimg" style={{ height: 250 }}>Insert Image</div>
                 <p>Non blandit massa enim nec dui nunc mattis enim. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla..</p>
                 <h3>Massa enim</h3>
                 <p>Lorem ipsum dolor sit ame.</p>
-                <div className="fakeimg" style={{ height: 60 }}>Image</div><br />
-                <div className="fakeimg" style={{ height: 60 }}>Image</div><br />
-                <div className="fakeimg" style={{ height: 60 }}>Image</div>
+                <div className="fakeimg" style={{ height: 110 }}>Insert Image</div><br />
+                <div className="fakeimg" style={{ height: 110 }}>Insert Image</div><br />
+                <div className="fakeimg" style={{ height: 110 }}>Insert Image</div>
             </div>
         )
     }
